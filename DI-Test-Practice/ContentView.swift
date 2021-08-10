8112275cf107b5e086cb5b8b710886f6623f69b3
iOSCS <dbrckstla@naver.com>
2021-08-10 20:26:03 +0900

    프로젝트 생성

---
+++ DI-Test-Practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DI-Test-Practice
+//
+//  Created by 육찬심 on 2021/08/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DI-Test-Practice/DI_Test_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DI_Test_PracticeApp.swift
+//  DI-Test-Practice
+//
+//  Created by 육찬심 on 2021/08/10.
+//
+
+import SwiftUI
+
+@main
+struct DI_Test_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
