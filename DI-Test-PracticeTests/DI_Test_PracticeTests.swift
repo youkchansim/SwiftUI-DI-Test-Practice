453cfd162b37aaaab96d905a68114d02a39816f9
iOSCS <dbrckstla@naver.com>
2021-08-10 20:36:15 +0900

    테스트 타겟 추가

---
+++ DI-Test-PracticeTests/DI_Test_PracticeTests.swift
@@ -0,0 +1,32 @@
+//
+//  DI_Test_PracticeTests.swift
+//  DI-Test-PracticeTests
+//
+//  Created by 육찬심 on 2021/08/10.
+//
+
+import XCTest
+
+class DI_Test_PracticeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
